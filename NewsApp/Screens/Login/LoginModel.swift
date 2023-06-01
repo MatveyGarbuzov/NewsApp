70f5bc26ad18ce4047f029615046b3415d29804f
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-06-01 11:56:53 +0300

    First commit

---
+++ NewsApp/Screens/Login/LoginModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginModel.swift
+//  NewsApp
+//
+//  Created by Matvey Garbuzov on 01.06.2023.
+//
+
+import Foundation
---
+++ NewsApp/Screens/Login/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  NewsApp
+//
+//  Created by Matvey Garbuzov on 01.06.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .red
+    setupConstraints()
+  }
+  
+  private func setupConstraints() {
+    
+  }
+}
